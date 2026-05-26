bdbc3ddfd21c42ddd061e6d3c11ee410245c38ed
Sparsh <sparshthapan@gmail.com>
2026-05-26 12:47:47 +0530

    added basic array solutions

---
+++ Arrays/Basics/RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,21 @@
+// LeetCode 26 - Remove Duplicates from Sorted Array
+// Pattern: Two Pointers
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
+
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+       int i =0;
+       for(int j =1; j<nums.length;j++){
+        if(nums[i] != nums[j]){
+            i++;
+            nums[i] = nums[j];
+        }
+       }
+       return i+1; 
+
+    }
+}
