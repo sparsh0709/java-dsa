07fbeb45da4be49c5b7325f886c0f9f13e2fbd99
Sparsh <sparshthapan@gmail.com>
2026-05-26 12:41:49 +0530

    added basic array solutions

---
+++ Arrays/Basics/TwoSum.java
@@ -0,0 +1,24 @@
+// LeetCode 1 - Two Sum Problem
+// Pattern: Basic
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            if (map.containsKey(complement)) {
+                return new int[]{ map.get(complement), i };
+            }
+            
+            map.put(nums[i], i);
+        }
+        
+        return new int[] {};
+    }
+}
